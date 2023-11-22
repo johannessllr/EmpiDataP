a572a3add30bad31909fb39b306517d97931425f
johannessllr <151741520+johannessllr@users.noreply.github.com>
2023-11-22 13:01:57 +0100

    Add files via upload

---
+++ empidataprojekt.R
@@ -0,0 +1,13 @@
+#Zusammenfassende Statistiken
+#1. Welcher Anteil der Betriebe in der Stichprobe bot den Mitarbeiter:innen im Jahr 2015 die Möglichkeit zur HO-Nutzung?
+
+#2. Welcher Anteil der Mitarbeiter:innen in den Betrieben, die HO anbieten, nutzt das HO auch?
+
+#3. Stellen Sie anhand einer geeigneten Tabelle oder Graphik dar, was die Gru ̈nde dafu ̈r sind, dass kein HO in Anspruch
+#genommen wird. Gehen Sie dabei auf die Ha ̈ufigkeit der gegebenen Antworten ein (z.B. durch ein Histogramm). Die dafu ̈r
+#beno ̈tigten Variablen finden Sie unter dem Variablennamen mheimnein
+
+#4. Welcher Anteil der Arbeitnehmer:innen in den Betrieben, die kein HO anbieten, ha ̈tte gerne die Mo ̈glichkeit, das HO zu nutzen?
+#5. Stellen Sie anhand einer geeigneten Tabelle oder Graphik dar, welche Gründe die Arbeitneh- mer:innen anfu ̈hren, warum Sie sich
+#HO wünschen. Gehen Sie dabei auf die Häufigkeit der ge- gebenen Antworten ein (z.B. durch ein Histogramm). Die dafu ̈r beno ̈tigte
+#n Variablen finden Sie unter dem Variablennamen mheimwunsch_ .
